bbc295eaea006bea6275864a798a50268e8be55c
Erik <erik@mail.com>
2024-01-30 14:50:32 +0200

    add in project files: .gitignore, .env, .env.example, dir: .vscode with file settings.json

--- project_survey/project_survey/settings.py
+++ project_survey/project_survey/settings.py
@@ -11,10 +11,14 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from os import getenv
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# Явно указываю путь до места хранения паролей
+load_dotenv(BASE_DIR / '.env')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
