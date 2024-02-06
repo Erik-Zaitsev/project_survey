b54ae8066568a871bf598476db1b15790cc97563
Erik <erik@mail.com>
2024-02-06 22:16:21 +0200

    small fix

--- book/views.py
+++ book/views.py
@@ -3,5 +3,5 @@ from django.views.generic import ListView
 from book.models import Book
 
 # Create your views here.
-class HomePage(ListView):
-    model = Book
+# class HomePage(ListView):
+#     model = Book
